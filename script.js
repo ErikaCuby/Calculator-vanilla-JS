6413dfd6be862c8372fd0fdeb646c048f0b33950
ErikaCuby <159401677+ErikaCuby@users.noreply.github.com>
2024-07-25 19:22:10 +0200

    Display clicked numbers from key-board

--- script.js
+++ script.js
@@ -7,8 +7,22 @@ const equal = document.querySelector(".equal");
 const clearAll = document.querySelector(".clear-all");
 const clearLastEntity = document.querySelector(".clear-last-entity");
 
-let dis1Num = "";
-let dis2Num = "";
+let disHistoryNum = "";
+let disCalculatorNum = "";
 let result = null;
 let lastOperation = "";
 let haveDot = false;
+
+//making sure, we gonna have only 1 dot in the number
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (e.target.innerText === "." && !haveDot) {
+      haveDot = true;
+    } else if (e.target.innerText === "." && haveDot) {
+      return;
+    }
+    //display numbers after clicking on keyboard
+    disCalculatorNum += e.target.innerText;
+    displayCalculator.innerText = disCalculatorNum;
+  });
+});
